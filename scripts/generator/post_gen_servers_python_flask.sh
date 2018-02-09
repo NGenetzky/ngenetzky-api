17b203b996c9cc37418aa95587ae71a3afdfb449
Nathan Genetzky <nathan@genetzky.us>
2018-02-08 21:38:02 -0600

    scripts/generator: Improve gen_servers

--- scripts/generator/post_gen_servers_python_flask.sh
+++ scripts/generator/post_gen_servers_python_flask.sh
@@ -1,10 +1,25 @@
-#!/bin/sh
+#!/bin/sh -x
 COMMIT="5858e03eab7971e6161915a3177738311c2650df"
 SWAGGER_URL="https://raw.githubusercontent.com/NGenetzky/ngenetzky-api/${COMMIT}/api/swagger.yaml"
 SERVER_PORT="80"
-curl -X POST \
-  "http://generator.swagger.io/api/gen/servers/python-flask" \
-  -H  "accept: application/json" \
-  -H  "content-type: application/json" \
-  -d "{  \"spec\": {},  \"options\": {    \"serverPort\": \"${SERVER_PORT}\"  },  \"swaggerUrl\": \"${SWAGGER_URL?}\"}" \
-  | jq .
+
+post_gen_servers()
+{
+  local gen_reply=$(curl -X POST \
+    "http://generator.swagger.io/api/gen/servers/python-flask" \
+    -H  "accept: application/json" \
+    -H  "content-type: application/json" \
+    -d "{  \"spec\": {},  \"options\": {    \"serverPort\": \"${SERVER_PORT}\"  },  \"swaggerUrl\": \"${SWAGGER_URL?}\"}" \
+    ) || return $?
+  echo $gen_reply | jq --raw-output .link
+}
+
+gen_servers()
+{
+  local filename="${1-archive.zip}"
+  local url=$(post_gen_servers)
+  wget --output-document "${filename}" "${url}"
+}
+
+gen_servers python-flask-generated.zip
+
